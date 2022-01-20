3d8055b8203df42979f52d801eb4fd0535dc09b1
Есин Андрей Борисович <Esin.A.Bo@sberbank.ru>
2022-01-20 20:07:47 +0300

    Works better

--- js/func.js
+++ js/func.js
@@ -1,43 +1,73 @@
+function draw(context) {
+
+    // Circle
+    context.fillStyle = "#ffffff";
+    context.beginPath();
+    context.arc(4, 12, 5, 0, 2 * Math.PI);
+    context.fill();
+
+    context.fillStyle = "#177305";
+    context.beginPath();
+    context.arc(4, 12, 3, 0, 2 * Math.PI);
+    context.fill();
+
+}
+
 function processIcon() {
-    var el = document.querySelectorAll('head link[rel*="icon"]');
-    //  if (!el) {
-    //       el = document.createElement("link");
-    //       el.setAttribute("rel", "shortcut icon");
-    //       document.head.appendChild(link);
-    //     }
+    var el = document.querySelectorAll('head link[rel~="icon"], head link[rel~="shortcut icon"]');
+
+    var faviconUrl = window.location.origin + "/favicon.ico";
+    
+    // var faviconUrl = 'chrome://favicon/' + request.url; // waiting https://developer.chrome.com/docs/extensions/mv3/intro/mv3-overview/
+    // https://docs.google.com/document/d/1ksvSq1zF-9jYSfGOvNluyoTV0Ud9xl6r9HiAKONqmz8/edit
+
     if (el) {
-
-        var faviconUrl = el.href || window.location.origin + "/favicon.ico";
-
-        // Remove existing favicons
-        Array.prototype.forEach.call(el, function (node) {
-            node.parentNode.removeChild(node);
-        });
-
-        var img = document.createElement("img");
-        img.addEventListener("load", function () {
-            var canvas = document.createElement("canvas");
-            canvas.width = 16;
-            canvas.height = 16;
-            var context = canvas.getContext("2d");
-            context.drawImage(img, 0, 0, 16, 16);
-
-            context.beginPath();
-            context.moveTo(0, 16);
-            context.lineTo(16, 16);
-            context.lineWidth = 4;
-            context.strokeStyle = '#ff0000';
-            context.stroke();
-
-            var link = document.createElement('link');
-            link.type = 'image/x-icon';
-            link.rel = 'icon';
-            link.href = canvas.toDataURL();
-
-            document.getElementsByTagName('head')[0].appendChild(link);
-        });
-        img.src = faviconUrl;
+        if (el.length > 0) {
+            faviconUrl = el[0].href;
+        }
     }
+
+    console.log(faviconUrl);
+    // Remove existing favicons
+    Array.prototype.forEach.call(el, function (node) {
+        node.parentNode.removeChild(node);
+    });
+
+    var canvas = document.createElement("canvas");
+    canvas.width = 16;
+    canvas.height = 16;
+    var context = canvas.getContext("2d");
+
+    var img = document.createElement("img");
+
+    img.addEventListener("load", function () {
+        context.drawImage(img, 0, 0, 16, 16);
+        draw(context);
+
+        var link = document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'icon';
+        link.href = canvas.toDataURL();
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+    });
+
+    // If something goes wrong with icon, just draw empty icon
+    img.addEventListener("error", function () {
+
+        context.clearRect(0, 0, 16, 16);
+        draw(context);
+
+        var link = document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'icon';
+        link.href = canvas.toDataURL();
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+    });
+
+    img.src = faviconUrl;
+
 
     return true;
 };
