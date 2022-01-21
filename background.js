0dba25f25a44ee7ab7cb2191741a8028f3999ef5
Есин Андрей Борисович <Esin.A.Bo@sberbank.ru>
2022-01-21 22:17:30 +0300

    1. Adding hotkey 2. Adding ability to click on extension menu to mark tab

--- background.js
+++ background.js
@@ -4,15 +4,16 @@ chrome.contextMenus.create({
 	contexts: ["all"]
 });
 
-// chrome.contextMenus.create({
-// 	id: "mark_tab_with_circle",
-// 	title: "With circle",
-// 	parentId: "mark_tab",
-// 	contexts:["all"]
-//   });
-  
-
 chrome.contextMenus.onClicked.addListener(menu => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.scripting.executeScript({
+			target: { tabId: tabs[0].id },
+			files: ['js/func.js'],
+		});
+	});
+});
+
+chrome.action.onClicked.addListener((tab) => {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		chrome.scripting.executeScript({
 			target: { tabId: tabs[0].id },
