039b5a6ca1a0bd632f1b416fd5a2bd468f15e589
Есин Андрей Борисович <Esin.A.Bo@sberbank.ru>
2022-01-23 00:38:17 +0300

    1. Try options 2. Try badges

--- js/background.js
+++ js/background.js
@@ -14,7 +14,11 @@ chrome.contextMenus.onClicked.addListener(menu => {
 });
 
 chrome.action.onClicked.addListener((tab) => {
+
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.action.setBadgeBackgroundColor({ color: '#ffffff' }, () => {
+			chrome.action.setBadgeText({ tabId: tabs[0].id, text: ' 🟢' });
+		});
 		chrome.scripting.executeScript({
 			target: { tabId: tabs[0].id },
 			files: ['js/func.js'],
